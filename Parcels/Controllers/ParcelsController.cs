e9cd95f6ada2cd5dd49fc5e024317c9bf5eb38e7
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-26 14:33:24 -0700

    Add routes to ParcelsController

--- Parcels/Controllers/ParcelsController.cs
+++ Parcels/Controllers/ParcelsController.cs
@@ -6,5 +6,21 @@ namespace Parcels.Controllers
 {
   public class ParcelsController : Controller
   {
+    [HttpGet("/parcels")]
+    public ActionResult Index()
+    {
+    return View();
+    }
+    [HttpGet("/parcels/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/parcels")]
+    public ActionResult Create(int length, int width, int height, int weight)
+    {
+      Parcel newParcel = new Parcel(length, width, height, weight);
+      return RedirectToAction("Index");
     }
   }
+}
