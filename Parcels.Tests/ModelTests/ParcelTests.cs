6e4529f5359083a5972f8fb18696423d2b18ea00
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-26 13:34:09 -0700

    Consolidate tests into 1 + pass test

--- Parcels.Tests/ModelTests/ParcelTests.cs
+++ Parcels.Tests/ModelTests/ParcelTests.cs
@@ -7,27 +7,12 @@ namespace Parcels.Tests
   public class ParcelsTests
   {
     [TestMethod]
-    public void Parcel_CreatesInstanceOfParcelAndChecksLength_Parcel()
+    public void Parcel_CreatesInstanceOfParcelAndChecksValuesAreCorrect_Parcel()
     {
       Parcel newParcel = new Parcel(10, 10, 10, 150);
       Assert.AreEqual(10, newParcel.Length);
-    }
-    [TestMethod]
-    public void Parcel_CreatesInstanceOfParcelAndChecksWidth_Parcel()
-    {
-      Parcel newParcel = new Parcel(10, 10, 10, 150);
       Assert.AreEqual(10, newParcel.Width);
-    }
-    [TestMethod]
-    public void Parcel_CreatesInstanceOfParcelAndChecksHeight_Parcel()
-    {
-      Parcel newParcel = new Parcel(10, 10, 10, 150);
       Assert.AreEqual(10, newParcel.Height);
-    }
-    [TestMethod]
-    public void Parcel_CreatesInstanceOfParcelAndChecksWeight_Parcel()
-    {
-      Parcel newParcel = new Parcel(10, 10, 10, 150);
       Assert.AreEqual(150, newParcel.Weight);
     }
   }
