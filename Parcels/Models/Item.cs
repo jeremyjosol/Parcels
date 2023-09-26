6979d57df47aacd7ef0ef2430013b10e88749591
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-26 12:57:26 -0700

    Add properties for Length, Width, and Height

--- Parcels/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Parcels.Models
-{
-  public class Parcel
-  {
-   
-  }
-}
---
+++ Parcels/Models/Parcel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Parcels.Models
+{
+  public class Parcel
+  {
+   public int Length { get; set; }
+   public int Width { get; set; }
+   public int Height { get; set; }
+  }
+}
